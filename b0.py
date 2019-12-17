174b5a2d03ed7878d25d661369970e657e43f033
michelestofella <48323978+michelestofella@users.noreply.github.com>
2019-12-17 15:02:25 +0000

    Updated

--- b0.py
+++ b0.py
@@ -1,12 +1,22 @@
-import numpy as np
+'''
+The script shows the dependence of initial condition B(0)
+on the exchange rates ki, kc and ko
+'''
 
-# NATIVE kc >> ko
-# EX1    ki >> kc 
-# EX2    ki << kc
-#ko  = 1e-6
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Paramters
+'''
+NATIVE kc >> ko
+EX1    ki >> kc 
+EX2    ki << kc
+'''
 ki = 1e-3
 kc  = np.logspace(-3,3,1000)*ki
 
+# Equation to calculate B(0) from ki, ko and kc 
 def init_b(ko,kc,ki):
     delta = np.sqrt((ko+kc+ki)**2 - 4*ki*ko)
     a2 = (ko+kc+ki - delta - (2*ki*ko)/(ki+ko)) / (2*delta)
@@ -16,11 +26,13 @@ def init_b(ko,kc,ki):
     b_0 = a2*u2 + a3*u3
     return b_0
 
+# Calculate B(0) from different conditions
 b0 = []
 for kc_v in kc:
     ko = kc_v/ki*1e-3
     b0.append(init_b(ko,kc_v,ki))
 
+# Plot the results
 plt.figure(figsize=(8,6))
 plt.rc('font', family='serif')
 plt.rc('xtick', labelsize='x-small')
